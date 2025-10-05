5bc3d4cdd59a6a7982e1cd3ee21910dfdb3c4ce5
Santosh26b <santoshbudagavi@gmail.com>
2025-10-05 13:44:20 +0530

    Removed dagshub implementation

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,19 +22,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
-import mlflow
 from urllib.parse import urlparse
-
-import dagshub
-
-# Read from environment variables
-dagshub_user = os.getenv("DAGSHUB_USER")
-
-dagshub.init(
-    repo_owner=dagshub_user,
-    repo_name='network-security',
-    mlflow=True
-)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -45,8 +33,7 @@ class ModelTrainer:
             raise NetworkSecurityException(e,sys)
         
     def track_mlflow(self,best_model,classificationmetric):
-        # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
-        # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        pass
         with mlflow.start_run():
             f1_score=classificationmetric.f1_score
             precision_score=classificationmetric.precision_score
@@ -130,12 +117,12 @@ class ModelTrainer:
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
         ## Track the experiements with mlflow
-        self.track_mlflow(best_model,classification_train_metric)
+        # self.track_mlflow(best_model,classification_train_metric)
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
 
-        self.track_mlflow(best_model,classification_test_metric)
+        # self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
